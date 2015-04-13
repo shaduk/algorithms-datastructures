4d83ab106ae18cd3be9569f1487cb7ec4e061c1c
Shad Ullah Khan <shaduk@gmail.com>
2015-04-13 11:45:44 +0000

    added a problem

---
+++ hackerrank/maxxor.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/maximizing-xor
+
+def  maxXor( l,  r):
+    max = 0
+    for i in range(l,r+1):
+        for j in range(l,r+1):
+            if i^j>max:
+                max = i^j
+    return max
+
+    
+
+_l = int(raw_input());
+
+
+_r = int(raw_input());
+
+res = maxXor(_l, _r);
+print(res)
