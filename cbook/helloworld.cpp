34a8acf61ce39df2522a2b5088447d388cb070a6
Shad Ullah Khan <shaduk@gmail.com>
2015-04-14 12:24:38 +0000

    started with C++

---
+++ cbook/helloworld.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main 
+{ 
+    cout << "Hello, World!\n"; // output “Hello, World!” 
+    return 0; 
+    
+    
+}
