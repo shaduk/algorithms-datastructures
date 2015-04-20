c95d572873e97a40847bb82e1a5edd603aa735f2
Shad Ullah Khan <shaduk@gmail.com>
2015-04-20 16:49:34 +0000

    C Book programs

---
+++ cbook/chap4drill.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main 
+{ 
+    int a,b;
+    string i;
+    cin>>i;
+    while(i!="|")
+    {
+        cin>>a>>b;
+        cout<<a<<"\n"<<b;
+        cin>>i;
+    }
+    
+}
