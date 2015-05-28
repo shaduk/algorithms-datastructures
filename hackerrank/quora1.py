419901d6458b2b245c871e534d9195a9973c1732
Shad Ullah Khan <shaduk@gmail.com>
2015-05-28 15:43:41 +0000

    upvote challenge updated

--- hackerrank/quora1.py
+++ hackerrank/quora1.py
@@ -1,19 +1,30 @@
 
-def upvotes(inlist):
-    count = 0
-    count2 = 0
+def score(n):
+    return n*(n-1)/2
+
+def noninc(inlist):
+    count = 1
+    scored = 0
     for i in range(0,len(inlist)-1):
-        if inlist[i+1]>inlist[i]:
-            count = count + 1
-        elif inlist[i+1]<inlist[i]:
-            count2 = count2 +1
+        if inlist[i+1]<=inlist[i]:
+            count = count +1            
         else:
-            count = count + 1
-            count2 = count2 +1
-    '''if count+1==len(inlist):
-        count = count+1'''
-    return count,count2 
-    
+            scored = scored + score(count)
+            count = 1
+    scored = scored + score(count)
+    return scored
+
+def nondec(inlist):
+    count = 1
+    scored = 0
+    for i in range(0,len(inlist)-1):
+        if inlist[i+1]>=inlist[i]:
+            count = count +1            
+        else:
+            scored = scored + score(count)
+            count = 1
+    scored = scored + score(count)
+    return scored
     
 n,k= raw_input().split(" ")
 n = int(n)
@@ -28,6 +39,5 @@ for i in range(0,n-k+1):
     for x in range(i,i+k):
         list1.append(int(li[x]))
     
-    print upvotes(list1)
+    print nondec(list1)-noninc(list1)
        
-        
