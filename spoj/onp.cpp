1c39150b725534065da5e8ccf4a03bb343d4deff
Shad Ullah Khan <shaduk@gmail.com>
2015-07-28 15:10:54 +0000

    added ONP from spoj

--- spoj/onp.cpp
+++ spoj/onp.cpp
@@ -1,24 +1,42 @@
-
-
 #include <iostream>
 #include <stack>
+#include <cctype>
+#include <cstring>
 using namespace std;
 
 int main()
 {
+    int testCase;
+    cin>>testCase;
+    char input[410];
+    std::stack<char> mystack;
     
-    string expression;
-    stack<char> mystack;
-    cin >> expression;
-    for(int i=0; i < expression.size(); i++)
+    while(testCase--)
     {
-        if(expression[i] == "(")
+        cin>>input;
+        int length = strlen(input);
+        for(int i = 0;i < length; i++)
         {
+            if(isalpha(input[i]))
+            {
+                cout << input[i];
+            }
             
+            else if ( input [i] == ')' ) 
+            {
+                cout << mystack.top();
+                mystack.pop();
+            }
+            
+            else if(input[i] != '(')
+            {
+                mystack.push(input[i]);
+            }
+           
         }
+    cout << endl;
+        
     }
-    
     return 0;
-    
 }
 
