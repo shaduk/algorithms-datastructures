a41824ca378f87314adc3985a3655d62f71847c7
Shad Ullah Khan <shaduk@gmail.com>
2015-07-29 10:36:01 +0000

    solved nsteps on spoj

---
+++ spoj/nsteps.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+     while(testCase--)
+    {
+        int x,y;
+        cin>> x >>y;
+        if(x%2!=0 && y%2!=0 && ((x-y)==2 || x-y==0))
+        {
+            cout << x+y-1<<"\n";
+        }
+        else if(x%2==0 && y%2==0 && ((x-y)==2 || x-y==0))
+        {
+            cout << x+y <<"\n";
+        }
+        else
+        {
+           
+            cout<<"No Number\n";
+        }
+        
+    }
+    return 0;
+}
