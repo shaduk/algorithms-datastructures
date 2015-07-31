f9ddbade6c274e4a03f32f150a06f35e2b539d4a
Shad Ullah Khan <shaduk@gmail.com>
2015-07-31 15:20:33 +0000

    Tried to and fro but failed, will try again

---
+++ spoj/toandfro.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+#include <cstring>
+
+
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int col,length=0,key;
+        
+        cin>>col;
+        
+        if(col == 0)
+        {
+            break;
+        }
+        string words;
+        cin.ignore();
+        key = 2*col;
+        in1 = 0;
+        in2 = key - 1;
+        std::getline(std::cin, words);
+        
+        for(int i=0;i < words.length(); i++)
+        {
+            
+           
+        }
+        
+        
+    }
+    return 0;
+}
