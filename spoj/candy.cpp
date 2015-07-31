a61366de41b7833587025af8e0e08cd59373f037
Shad Ullah Khan <shaduk@gmail.com>
2015-07-31 16:34:45 +0000

    solved candy on spoj

---
+++ spoj/candy.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase=0;
+    
+    while(testcase!= -1)
+    {
+    cin >> testcase;
+    if(testcase == -1)
+    {
+        return 0;
+    }
+    int inputs[testcase],sum=0,ans=0,avg=0;
+    for(int i = 0; i < testcase;i++)
+    {
+        cin >> inputs[i]; 
+        sum = sum + inputs[i];
+    }
+    
+    avg = sum/testcase;
+   
+    
+    if(sum%testcase!=0)
+    {
+        cout  << -1 << "\n";
+    }
+    else
+    {
+        for(int i = 0; i < testcase;i++)
+        {
+            if(inputs[i]>=avg)
+            {
+                 ans = ans + (inputs[i]-avg);
+            }
+        }
+        cout << ans << "\n";
+    }
+    
+    }
+    return 0;
+}
