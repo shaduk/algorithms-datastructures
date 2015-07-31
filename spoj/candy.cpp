01f373ec3cfd67269b967e27293066ef1a82d761
Shad Ullah Khan <shaduk@gmail.com>
2015-07-31 16:36:14 +0000

    added candy

--- spoj/candy.cpp
+++ spoj/candy.cpp
@@ -7,36 +7,37 @@ int main()
     
     while(testcase!= -1)
     {
-    cin >> testcase;
-    if(testcase == -1)
-    {
-        return 0;
-    }
-    int inputs[testcase],sum=0,ans=0,avg=0;
-    for(int i = 0; i < testcase;i++)
-    {
-        cin >> inputs[i]; 
-        sum = sum + inputs[i];
-    }
+        cin >> testcase;
+        if(testcase == -1)
+        {
+            return 0;
+        }
+        int inputs[testcase],sum=0,ans=0,avg=0;
     
-    avg = sum/testcase;
-   
-    
-    if(sum%testcase!=0)
-    {
-        cout  << -1 << "\n";
-    }
-    else
-    {
         for(int i = 0; i < testcase;i++)
         {
-            if(inputs[i]>=avg)
-            {
-                 ans = ans + (inputs[i]-avg);
-            }
+            cin >> inputs[i]; 
+            sum = sum + inputs[i];
         }
-        cout << ans << "\n";
-    }
+    
+        avg = sum/testcase;
+   
+    
+        if(sum%testcase!=0)
+        {
+            cout  << -1 << "\n";
+        }   
+        else
+        {
+            for(int i = 0; i < testcase;i++)
+            {
+                if(inputs[i]>=avg)
+                {
+                    ans = ans + (inputs[i]-avg);
+                }
+            }
+            cout << ans << "\n";
+        }
     
     }
     return 0;
