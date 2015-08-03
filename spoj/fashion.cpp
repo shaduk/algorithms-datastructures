fc6fc7b387f0659ed6268f7d13f0dbd8171d85da
Shad Ullah Khan <shaduk@gmail.com>
2015-08-03 11:30:43 +0000

    Added problem fashion from spoj

--- spoj/fashion.cpp
+++ spoj/fashion.cpp
@@ -7,28 +7,28 @@ int main()
      int testcases;
      cin>>testcases;
      while(testcases--)
-     {
-     int n,bonds=0;
-     cin >> n;
-     int male[n],female[n];
-     for(int i=0;i < n; i++)
-     {
-         cin >> male[i];
+    {
+        int n,bonds=0;
+        cin >> n;
+        int male[n],female[n];
+        for(int i=0;i < n; i++)
+        {
+            cin >> male[i];
      
-     }
-     for(int i=0;i < n; i++)
-     {
-         cin >> female[i];
+        }
+        for(int i=0;i < n; i++)
+        {
+            cin >> female[i];
      
-     }
+        }
      
-     sort(male,male + n);
-     sort(female,female + n);
+        sort(male,male + n);
+        sort(female,female + n);
      
-     for(int i=0;i < n; i++)
-     {
-        bonds = bonds + male[i]*female[i];
-     }
-    cout << bonds <<"\n";
+        for(int i=0;i < n; i++)
+        {
+            bonds = bonds + male[i]*female[i];
+        }
+        cout << bonds <<"\n";
      }
 }
