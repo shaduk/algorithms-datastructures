36b1bc747a0015e70085ad8b4604e4e5727eb756
Shad Ullah Khan <shaduk@gmail.com>
2016-02-08 07:34:30 +0000

    improved magical shop eprformce

--- hackerearth/magicalshop.cpp
+++ hackerearth/magicalshop.cpp
@@ -3,36 +3,24 @@
 using namespace std;
 
 
-int Mod(int x, int n, int mod)
-{
-	if(n == 0)
-	return 1;
-	else if(n%2 == 0)
-	{
-		int y = Mod(x, n/2, mod);
-		return y*y;
-	}
-	else
-	{
-		return ((x%mod) * Mod(x, n-1, mod) % mod);
-	}
-}
 
 int main()
 {
-	int x, mod, n = 1, total = 0;
+	long long int x, mod, total = 0;
 	string line;
     cin >> x;
     cin >> mod;
     cin >> line;
     for(int i = 0; i < line.length(); i++)
     {
-    	
+   
     	if(line[i] == '1')
     	{
-    		total = (total + Mod(x, n, mod))%mod;
+    		total = (total%mod + x%mod)%mod;
     	}
-    	n = n * 2;
+    	long long int temp = ((x%mod)*(x%mod))%mod;
+    	x = temp;
+    
     }
     cout << total;
 }
