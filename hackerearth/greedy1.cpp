c65d3dbdb38d9fa81318a8c02176c506d2136c04
Shad Ullah Khan <shaduk@gmail.com>
2016-03-31 12:59:50 +0000

    solved prob on hackerearth using greedy

--- hackerearth/greedy1.cpp
+++ hackerearth/greedy1.cpp
@@ -10,12 +10,21 @@ int main()
    cin >> test;
    while(test--)
    {
-   		string str1, str2, ans;
+   		string str1, str2, ans= "";
    		cin >> str1 >> str2;
    		sort(str2.begin(), str2.end());
    		for(int i = 0; i < str1.size(); i++)
    		{
-   			
+   			if(str2.size() >= 1 && str2[0] < str1[i])
+   			{
+   				ans = ans + str2[0];
+   				str2 = str2.substr(1);
+   			}
+   			else
+   			{
+   				ans = ans + str1[i];
+   			}
    		}
+   		cout << ans << endl;
    }
 }
