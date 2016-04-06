fd01dd63d8370ca23bb7bc6528318ed817d5d689
Shad Ullah Khan <shaduk@gmail.com>
2016-04-06 13:29:09 +0000

    cleaned code for insert at tail

--- hackerrank/DS/insertendlist.cpp
+++ hackerrank/DS/insertendlist.cpp
@@ -10,27 +10,21 @@
 */
 Node* Insert(Node *head,int data)
 {
+      Node* temp = new Node();
+      temp->data = data;
+      temp->next = NULL;
+    
     if(head == NULL)
      {
-        Node* temp3 = new Node();
-        temp3->data = data;
-        temp3->next = NULL; 
-         
-        Node *temp = head;
-        temp = temp3;
-        return temp;
+        head = temp;
+        return head;
     }
     else
     {
-        Node* temp2 = new Node();
-        temp2->data = data;
-        temp2->next = NULL;
-        
-        Node* temp1;
-        temp1 = head;
+        Node* temp1 = head;
         while(temp1->next != NULL)
             temp1 = temp1->next;
-        temp1->next = temp2;
-            return head;
+        temp1->next = temp;
+        return head;
     }
 }
