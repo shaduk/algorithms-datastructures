c77abdaa4d9c8d2d829a502aec0fd3604d679866
Shad Ullah Khan <shaduk@gmail.com>
2016-04-13 10:35:31 +0000

    added code to find height of binary search tree

---
+++ hackerrank/DS/HeightTree.cpp
@@ -0,0 +1,30 @@
+
+/*The tree node has data, left child and right child 
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+int height(node * root)
+{
+    
+    if(root)
+        {
+        
+          int left = height(root->left);
+          int right = height(root->right);
+              
+          if( left > right)
+              {
+              return 1 + left;
+          }
+          else
+              return 1 + right;       
+    }
+    else
+        return -1;
+}
+  
