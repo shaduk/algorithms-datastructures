26b8fe61f670e1a61c58057241564c404a66a428
Shad Ullah Khan <shaduk@gmail.com>
2016-04-25 20:59:12 +0000

    added solution to spoj problem ambiguous permutations

--- spoj/permut2.cpp
+++ spoj/permut2.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int test;
-    cin >> test;
-    while(test != 0)
+    int N;
+    cin >> N;
+    while(N != 0)
     {
+        bool flag = true;
+        int no[N];
+        int inv[N];
         
+        for(int i = 1; i <= N; i++)
+        {
+            int n;
+            cin >> n;
+            no[i] = n;
+        }
+        
+        for(int i = 1; i <= N; i++)
+        {
+            inv[no[i]] = i; 
+        }
+        for(int i = 1; i <= N; i++)
+        {
+            if(no[i] != inv[i])
+            flag = false;
+        }
+        if(flag)
+        cout << "ambiguous" << endl;
+        else
+        cout << "not ambiguous" << endl;
+        cin >> N;
     }
 }
