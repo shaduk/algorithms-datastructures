0c62c929e6fe2f1f72fd8d2bd22f3886bc08ca95
Shad Ullah Khan <shaduk@gmail.com>
2016-04-27 12:18:03 +0000

    spoj practice

---
+++ spoj/stpar.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    while(N != 0)
+    {
+        stack<int> mystack1, mystack2;
+        for(int i = 0; i < N; i++)
+        {
+            int input;
+            cin >> input;
+            if(mystack1.empty() && input == 1)
+                mystack1.push(input);
+            else if(!mystack1.empty() && (input-mystack1.top() == 1))
+                mystack1.push(input);
+            else
+                mystack2.push(input);
+            if(!mystack2.empty() && !mystack1.empty() && mystack2.top()-mystack1.top()==1)
+            {
+                mystack1.push(mystack2.top());
+                mystack2.pop();
+            }
+        }
+        
+        while(!mystack2.empty() && mystack2.top()-mystack1.top() == 1)
+        {
+            mystack1.push(mystack2.top());
+            mystack2.pop();
+        }
+        
+        while(!mystack1.empty())
+        {
+            cout << mystack1.top() << " ";
+            mystack1.pop();
+        }
+        cout << endl;
+        cin >> N;
+    }
+}
