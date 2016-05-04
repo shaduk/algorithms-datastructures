19cda55e74d7f40f027074f9948dcc7409d04f43
Shad Ullah Khan <shaduk@gmail.com>
2016-05-04 23:09:37 +0000

    hackerrank practice

---
+++ hackerrank/LisaBook.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, k, ans = 0;
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++)
+        {
+        
+    }
+    return 0;
+}
