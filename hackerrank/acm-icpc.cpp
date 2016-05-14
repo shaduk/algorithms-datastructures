24294549cd415ced0d2a34adf85819ccc07e6d4e
Shad Ullah Khan <shaduk@gmail.com>
2016-05-14 23:21:22 +0000

    acm icpc prob on hackerrank

---
+++ hackerrank/acm-icpc.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+    cin >> n >> m;
+    vector<string> topic(n);
+    for(int topic_i = 0;topic_i < n;topic_i++){
+       cin >> topic[topic_i];
+    }
+    return 0;
+}
