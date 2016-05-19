21cf78d51af2feb886b4fd69b011d1ee3eb5a026
Shad Ullah Khan <shaduk@gmail.com>
2016-05-19 23:21:17 +0000

    hackeearth practice

--- hackerearth/dhoom4.cpp
+++ hackerearth/dhoom4.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main()
 {
    	int sval, lval, N;
+   	queue<int> myq;
    	cin >> sval >> lval;
    	cin >> N;
+   	for(int i = 0; i < N; i++)
+   	{
+   		int input;
+   		cin >> input;
+   		myq.push(input);
+   	}
    	
+   	while(sval)
 }
