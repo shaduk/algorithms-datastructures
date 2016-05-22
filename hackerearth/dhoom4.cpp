c46ddf039208f324ec38633df6a25ba9e54d1a6d
Shad Ullah Khan <shaduk@gmail.com>
2016-05-22 14:20:04 +0000

    hackerearth practice

--- hackerearth/dhoom4.cpp
+++ hackerearth/dhoom4.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 #include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   	int sval, lval, N;
+   	long long int sval, lval, N;
    	queue<int> myq;
    	cin >> sval >> lval;
    	cin >> N;
+   	int Arr[N];
+   	int ans[100005];
+	memset(ans,-1,sizeof(ans));
    	for(int i = 0; i < N; i++)
    	{
-   		int input;
+   		long long int input;
    		cin >> input;
-   		myq.push(input);
+   		Arr[i] = input;
    	}
    	
-   	while(sval)
+   	myq.push(sval);
+   	ans[sval] = 0;
+   	while(!myq.empty())
+   	{
+   		long long int current = myq.front();
+   		myq.pop();
+   		if(current == lval)
+   			break;
+   		for(int i = 0; i < N; i++)
+   		{
+   			long long int to = Arr[i];
+   			to = to*current;
+   			to = to%100000;
+   			if(ans[to] == -1)
+   			{
+   				ans[to] = ans[current] + 1;
+   				myq.push(to);
+   			}
+   		}
+   	}
+   	
+   	cout << ans[lval] << endl;
+   	return 0;
 }
